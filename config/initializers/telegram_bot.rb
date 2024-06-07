f997beb0d63c63b6af3e3c03ed2b61f7fac73daa
Deanout <dehartdean@gmail.com>
2024-06-07 13:56:00 -0400

    Add Open AI Stuff

--- config/initializers/telegram_bot.rb
+++ config/initializers/telegram_bot.rb
@@ -1,20 +1,16 @@
 require 'telegram/bot'
 
 Rails.application.config.after_initialize do
+  puts "Running Telegram Bot Job!"
+  TelegramBotWorkerJob.perform_async(Rails.application.credentials.dig(:telegram_bot))
+end
+
+Rails.application.config.after_initialize do
   token = Rails.application.credentials.dig(:telegram_bot)
 
   Telegram::Bot::Client.run(token) do |bot|
     Rails.application.config.telegram_bot = bot
     bot.api.get_updates(offset: -1)
-    bot.listen do |message|
-      next unless message.is_a?(Telegram::Bot::Types::Message)
-      case message.text
-      when '/start'
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} | Chat ID: #{message.chat.id}")
-      when '/stop'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name} | Chat ID: #{message.chat.id}")
-      end
-    end
   end
 rescue Telegram::Bot::Exceptions::ResponseError => e
   Rails.logger.error e
